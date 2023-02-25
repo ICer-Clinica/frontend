24546d15b29718a649fe0790cde32f2d03e0bd03
guiisn <guilhermesouza96910524@gmail.com>
2023-02-25 13:08:49 -0300

    chore: add clinic adm dashboard

--- src/pages/ClinicAdm/Dashboard/index.tsx
+++ src/pages/ClinicAdm/Dashboard/index.tsx
@@ -1,3 +1,173 @@
+import { Box, Grid } from "@mui/material";
+import Chart from "react-google-charts";
+import { useQuery } from "react-query";
+import DashboardCard from "../../../components/atoms/DashboardCard";
+import { ApiService } from "../../../config/api";
+import { clinicAdmEndpoints } from "../../../utils/endpoints/clinicAdm";
+import { getClinicID } from "../../../utils/functions/GetClinicID";
+
 export default function Dashboard() {
-  return <div>info sobre a clínica</div>;
+  const getRanking = async () => {
+    const api = new ApiService();
+    return api.RequestData(
+      "GET",
+      clinicAdmEndpoints.rankingOfTherapists(getClinicID())
+    );
+  };
+  const getRankingThisMonth = async () => {
+    const api = new ApiService();
+    return api.RequestData(
+      "GET",
+      clinicAdmEndpoints.rankingOfTherapistsThisMonth(getClinicID())
+    );
+  };
+  const getTotalOfTherapists = async () => {
+    const api = new ApiService();
+    return api.RequestData(
+      "GET",
+      clinicAdmEndpoints.totalOfTherapists(getClinicID())
+    );
+  };
+  const getTotalOfPatients = async () => {
+    const api = new ApiService();
+    return api.RequestData(
+      "GET",
+      clinicAdmEndpoints.totalOfPatients(getClinicID())
+    );
+  };
+  const getTotalOfAttendances = async () => {
+    const api = new ApiService();
+    return api.RequestData(
+      "GET",
+      clinicAdmEndpoints.totalOfAttendances(getClinicID())
+    );
+  };
+  const getTotalOfAttendancesThisMonth = async () => {
+    const api = new ApiService();
+    return api.RequestData(
+      "GET",
+      clinicAdmEndpoints.totalOfAttendancesThisMonth(getClinicID())
+    );
+  };
+
+  const { isLoading: getRankingIsLoading, data: ranking } = useQuery(
+    "ranking",
+    getRanking
+  );
+  const { isLoading: getRankingThisMonthIsLoading, data: rankingThisMonth } =
+    useQuery("rankingThisMonth", getRankingThisMonth);
+  const { isLoading: getTotalTherapists, data: totalOfTherapists } = useQuery(
+    "totalOfTherapists",
+    getTotalOfTherapists
+  );
+  const { isLoading: getTotalPatients, data: totalOfPatients } = useQuery(
+    "totalOfPatients",
+    getTotalOfPatients
+  );
+  const { isLoading: getTotalAttendances, data: totalOfAttendances } = useQuery(
+    "totalOfAttendances",
+    getTotalOfAttendances
+  );
+  const {
+    isLoading: getTotalAttendancesThisMonth,
+    data: totalOfAttendancesThisMonth,
+  } = useQuery("totalOfAttendancesThisMonth", getTotalOfAttendancesThisMonth);
+
+  const rankingOfTherapists = ranking as any[];
+
+  const formattedRankingOfTherapists = [
+    [
+      "Element",
+      "Terapeutas com mais atendimentos realizados",
+      { role: "style" },
+    ],
+  ];
+
+  rankingOfTherapists?.forEach((therapist) => {
+    formattedRankingOfTherapists.push([
+      therapist?.therapistName,
+      therapist?.attendances,
+      "#F84B5A",
+    ]);
+  });
+
+  const rankingOfTherapistsThisMonth = rankingThisMonth as any[];
+
+  const formattedRankingOfTherapistsThisMonth = [
+    [
+      "Element",
+      "Terapeutas com mais atendimentos realizados neste mês",
+      { role: "style" },
+    ],
+  ];
+
+  rankingOfTherapistsThisMonth?.forEach((therapist) => {
+    formattedRankingOfTherapistsThisMonth.push([
+      therapist?.therapistName,
+      therapist?.attendances,
+      "#F84B5A",
+    ]);
+  });
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        gap: "1.5rem",
+        height: "100%",
+      }}
+    >
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <DashboardCard
+            title="Total de terapeutas"
+            value={Number(totalOfTherapists)}
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <DashboardCard
+            title="Total de pacientes"
+            value={Number(totalOfPatients)}
+          />
+        </Grid>
+      </Grid>
+
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <DashboardCard
+            title="Atendimentos realizados"
+            value={Number(totalOfAttendances)}
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <DashboardCard
+            title="Atendimentos realizados nesse mês"
+            value={Number(totalOfAttendancesThisMonth)}
+          />
+        </Grid>
+      </Grid>
+
+      <Grid container spacing={2} sx={{ height: "100%", maxHeight: "100%" }}>
+        <Grid item xs={6}>
+          <Chart
+            chartType="ColumnChart"
+            width="100%"
+            height="100%"
+            data={formattedRankingOfTherapists}
+          />
+        </Grid>
+        <Grid item xs={6}>
+          <Chart
+            chartType="ColumnChart"
+            width="100%"
+            height="100%"
+            data={formattedRankingOfTherapistsThisMonth}
+          />
+        </Grid>
+      </Grid>
+    </Box>
+  );
 }
+
--- src/utils/endpoints/clinicAdm/index.ts
+++ src/utils/endpoints/clinicAdm/index.ts
@@ -12,4 +12,11 @@ export const clinicAdmEndpoints = {
   createCoordinators: "/coordinators",
   listAllAdmSecretaries: (clinic_id: string) => `/adm-secretary/${clinic_id}`,
   createAdmSecretary: "/adm-secretary",
+
+  totalOfTherapists: (clinic_id: string) => `/total-of-therapists/${clinic_id}`,
+  totalOfPatients: (clinic_id: string) => `/total-of-patients/${clinic_id}`,
+  totalOfAttendances: (clinic_id: string) => `/total-of-attendances/${clinic_id}`,
+  totalOfAttendancesThisMonth: (clinic_id: string) => `/total-of-attendances/${clinic_id}/this-month`,
+  rankingOfTherapists: (clinic_id: string) => `/ranking/therapists/${clinic_id}`,
+  rankingOfTherapistsThisMonth: (clinic_id: string) => `/ranking/therapists/${clinic_id}/this-month`,
 };
