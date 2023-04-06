38b958d452f07ed063ea89c1b426f2f84e6b0508
guisnunex <guilhermesouza96910524@gmail.com>
2023-04-06 11:17:17 -0300

    fix: listing components

---
+++ src/components/Listings/CoordinatorListing/index.tsx
@@ -0,0 +1,121 @@
+import { Icon } from "@iconify/react";
+import { Tooltip } from "@mui/material";
+import { useState } from "react";
+import { useMutation, useQuery, useQueryClient } from "react-query";
+import { removeCoordinators } from "../../../components/atoms/CardList/request";
+import ModalConfirm from "../../../components/atoms/ModalConfirm";
+import Listing from "../../../components/molecules/Listing";
+import { ApiService } from "../../../config/api";
+import { clinicAdmEndpoints } from "../../../utils/endpoints/clinicAdm";
+import { getClinicID } from "../../../utils/functions/GetClinicID";
+import { humanizeTypes } from "../../../utils/functions/Humanize";
+
+interface ICoordinatorsListing {
+    entity: 'clinicAdm'
+}
+
+export default function CoordinatorsListing({entity}: ICoordinatorsListing) {
+  const queryClient = useQueryClient()
+  const [coordinators, setCoordinators] = useState([]);
+  const [open, setOpen] = useState({
+    opened: false,
+    id: "",
+  });
+  const handleOpen = (id: string) => setOpen({ opened: true, id });
+  const handleClose = () => setOpen({ opened: false, id: "" });
+
+  const fetchCoordinators = async () => {
+    const api = new ApiService();
+    return api.RequestData(
+      "GET",
+      clinicAdmEndpoints.listAllCoordinators(getClinicID())
+    );
+  };
+
+  const { isLoading } = useQuery("coordinators", fetchCoordinators, {
+    onSuccess(data: any) {
+      setCoordinators(data);
+    },
+    onError(err) {
+      console.log(err);
+    },
+  });
+
+  const { mutate, isLoading: mutateIsLoading } = useMutation(
+    removeCoordinators,
+    {
+      onSuccess: (data: any) => {
+        setOpen({...open, opened: false})
+        queryClient.invalidateQueries(['coordinators'])
+      },
+      onError: (error) => {
+        alert(error);
+      },
+    }
+  );
+
+  const clickDelete = () => {
+    mutate(open.id);
+  };
+
+  const rows: any = [];
+  coordinators &&
+    coordinators?.forEach((clinic: any) => {
+      const data = {
+        name: clinic?.name,
+        email: clinic?.email,
+        created_at: new Date(clinic?.created_at)?.toLocaleDateString("pt-br"),
+        actions: (
+          <Tooltip title="Remover">
+            <button
+              onClick={() => handleOpen(clinic?.id)}
+              style={{
+                width: 40,
+                height: 40,
+                color: "white",
+                backgroundColor: "#DD404D",
+                borderRadius: "100%",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                outline: "none",
+                border: 0,
+                cursor: "pointer",
+              }}
+            >
+              <Icon icon="ic:baseline-delete-forever" width={30} />
+            </button>
+          </Tooltip>
+        ),
+      };
+      rows.push(data);
+    });
+
+  return (
+    <>
+      <Listing
+        textButton="Cadastrar Coordenador"
+        title="Coordenadores"
+        data={coordinators}
+        type="coordinators"
+        isLoading={isLoading}
+        link={`/${entity}/coordinators/create`}
+        columns={[
+          { label: "Nome", value: "name" },
+          { label: "E-mail", value: "email" },
+          { label: "Criado em", value: "created_at" },
+          { label: "Ações", value: "actions" },
+        ]}
+        rows={rows}
+      />
+      <ModalConfirm
+        handleClose={handleClose}
+        open={open.opened}
+        clickConfirm={clickDelete}
+        text="Tem certeza que deseja excluir?"
+        title={humanizeTypes("clinic")}
+        isLoading={mutateIsLoading}
+      />
+    </>
+  );
+}
---
+++ src/components/Listings/Patients/index.tsx
@@ -0,0 +1,130 @@
+import { Icon } from "@iconify/react";
+import { Tooltip } from "@mui/material";
+import { useState } from "react";
+import { useMutation, useQuery } from "react-query";
+import {
+  removePatients,
+  removeProcedures,
+} from "../../../components/atoms/CardList/request";
+import ModalConfirm from "../../../components/atoms/ModalConfirm";
+import Listing from "../../../components/molecules/Listing";
+import { ApiService } from "../../../config/api";
+import { clinicAdmEndpoints } from "../../../utils/endpoints/clinicAdm";
+import { getClinicID } from "../../../utils/functions/GetClinicID";
+import { humanizeTypes } from "../../../utils/functions/Humanize";
+import {
+  humanizeCellphone,
+  humanizeCPF,
+  humanizeSusCard,
+} from "../../../utils/functions/humanizers";
+
+interface IPatientsListing {
+    entity: 'clinicAdm' | 'coordinator' | 'admnistrativeSecretary'
+}
+
+export default function PatientsListing({entity}: IPatientsListing) {
+    const [patients, setPatients] = useState([]);
+  const [open, setOpen] = useState({
+    opened: false,
+    id: "",
+  });
+  const handleOpen = (id: string) => setOpen({ opened: true, id });
+  const handleClose = () => setOpen({ opened: false, id: "" });
+
+  const fetchPatients = async () => {
+    const api = new ApiService();
+    return api.RequestData(
+      "GET",
+      clinicAdmEndpoints.listAllPatients(getClinicID())
+    );
+  };
+
+  const { isLoading } = useQuery("patients", fetchPatients, {
+    onSuccess(data: any) {
+      setPatients(data);
+    },
+    onError(err) {
+      alert(err);
+    },
+  });
+
+  const { mutate, isLoading: mutateIsLoading } = useMutation(removePatients, {
+    onSuccess: (data: any) => {
+      window.location.reload();
+    },
+    onError: (error) => {
+      alert(error);
+    },
+  });
+
+  const clickDelete = () => {
+    mutate(open.id);
+  };
+
+  const rows: any = [];
+  patients &&
+    patients?.forEach((clinic: any) => {
+      const data = {
+        name: clinic?.name,
+        susCard: humanizeSusCard(clinic?.sus_card),
+        cpf: humanizeCPF(clinic?.cpf),
+        phone: humanizeCellphone(clinic?.phone),
+        birthDate: new Date(clinic?.birth_date)?.toLocaleDateString(),
+        created_at: new Date(clinic?.created_at)?.toLocaleDateString("pt-br"),
+        actions: (
+          <Tooltip title="Remover">
+            <button
+              onClick={() => handleOpen(clinic?.id)}
+              style={{
+                width: 40,
+                height: 40,
+                color: "white",
+                backgroundColor: "#DD404D",
+                borderRadius: "100%",
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+                outline: "none",
+                border: 0,
+                cursor: "pointer",
+              }}
+            >
+              <Icon icon="ic:baseline-delete-forever" width={30} />
+            </button>
+          </Tooltip>
+        ),
+      };
+      rows.push(data);
+    });
+
+  return (
+    <>
+      <Listing
+        textButton="Cadastrar Paciente"
+        title="Pacientes"
+        data={patients}
+        type="patients"
+        isLoading={isLoading}
+        link={`/${entity}/patients/create`}
+        columns={[
+          { label: "Nome", value: "name" },
+          { label: "Cartão SUS", value: "susCard" },
+          { label: "CPF", value: "cpf" },
+          { label: "Telefone", value: "phone" },
+          { label: "Data nascimento", value: "birthDate" },
+          { label: "Criado em", value: "created_at" },
+          { label: "Ações", value: "actions" },
+        ]}
+        rows={rows}
+      />
+      <ModalConfirm
+        handleClose={handleClose}
+        open={open.opened}
+        clickConfirm={clickDelete}
+        text="Tem certeza que deseja excluir?"
+        title={humanizeTypes("clinic")}
+        isLoading={mutateIsLoading}
+      />
+    </>
+  );
+}
